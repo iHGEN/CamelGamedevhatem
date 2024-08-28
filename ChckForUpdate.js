a3de15a42c05d91e41e1f7d1f95722ccde68a23b
Hatem <daimn.mekasa@gmail.com>
2024-08-28 22:51:48 +0300

    scrennshot test

--- ChckForUpdate.js
+++ ChckForUpdate.js
@@ -1,7 +1,7 @@
 const versionKey = 'GameVersion';
 const dbName = 'AraiCache';
 const storeName = 'cacheStore';
-const newVersion = '1.7'; 
+const newVersion = '1.8'; 
 
 async function getDB() {
     return new Promise((resolve, reject) => {
