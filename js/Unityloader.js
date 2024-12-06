8827bc014e42f5ddd4ac13501e9aa22370dd6e4e
Hatem <daimn.mekasa@gmail.com>
2024-12-07 02:00:08 +0300

    fix file name

--- js/Unityloader.js
+++ js/Unityloader.js
@@ -28,9 +28,9 @@ async function loadUnityGame() {
 
   // Configuration for Unity instance
   var config = {
-    dataUrl: buildUrl + "/CamelGameDevHatem.data.br", 
-    frameworkUrl: buildUrl + "/CamelGameDevHatem.framework.js.br",
-    codeUrl: buildUrl + "/CamelGameDevHatem.wasm.br",
+    dataUrl: buildUrl + "/CamelGameDevاatem.data.br", 
+    frameworkUrl: buildUrl + "/CamelGameDevاatem.framework.js.br",
+    codeUrl: buildUrl + "/CamelGameDevاatem.wasm.br",
     streamingAssetsUrl: "StreamingAssets",
     companyName: "DefaultCompany",
     productName: "i20-Project",
@@ -50,7 +50,7 @@ async function loadUnityGame() {
 
   // Load the Unity loader script
   var script = document.createElement("script");
-  script.src = buildUrl + "/CamelGameDevHatem.loader.js";
+  script.src = buildUrl + "/CamelGameDevاatem.loader.js";
   script.onload = () => {
     createUnityInstance(canvas, config, (progress) => {
           progressBarFull.style.width = 100 * progress + "%";
